ddd23249261fde42905820dc78201d5dfde2b2ff
Jeganathan Swaminathan <mail2jegan@gmail.com>
2021-06-08 00:00:19 -0700

    Added CRM project with sonarqube configurations.

---
+++ Day2/CRM/main/src/test/java/org/tektutor/MainTest.java
@@ -0,0 +1,20 @@
+package org.tektutor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+
+		Main main = new Main();
+
+		String actualResponse   = main.getModuleName();
+		String expectedResponse = "Main Module";
+
+		assertEquals ( expectedResponse, actualResponse );
+
+	}
+
+}
